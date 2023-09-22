c5f877abffbb307a1c0d583146e6a75b11538660
cloud_du <cloud_du@accton.com>
2023-09-22 15:25:42 +0800

    [FuncAdd][FirstCommit] First Commit

---
+++ lib/package_test.dart
@@ -0,0 +1,17 @@
+library package_test;
+
+import 'package:flutter/material.dart';
+
+class TestReportPage extends StatelessWidget {
+  const TestReportPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      height: double.infinity,
+      child: Center(
+        child: Text("測試報表"),
+      ),
+    );
+  }
+}
