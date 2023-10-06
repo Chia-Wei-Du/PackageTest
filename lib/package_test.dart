698c59c3b8fa11423b1baa104393ea019a7e5e63
cloud_du <cloud_du@accton.com>
2023-10-06 17:04:05 +0800

    設定isDevMode動態判斷是否為Dev Mode

--- lib/package_test.dart
+++ lib/package_test.dart
@@ -3,15 +3,16 @@ library package_test;
 import 'package:flutter/material.dart';
 
 class TestReportPage extends StatelessWidget {
-  const TestReportPage({super.key});
+  final bool? isDevMode;
+  const TestReportPage({super.key, this.isDevMode = false});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SizedBox(
         height: double.infinity,
         child: Center(
-          child: Text("測試畫面 From Git"),
+          child: Text("測試畫面 From Git\nDev mode = $isDevMode"),
         ),
       ),
     );
